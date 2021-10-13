8e6eea9cf7d0070b10684a8d108a4d60d4b461b9
fajar <harisyahfajars9@gmail.com>
2021-10-13 22:10:40 +0800

    home ui not fix

--- src/Screen/Home/Home.js
+++ src/Screen/Home/Home.js
@@ -1,43 +1,146 @@
-import React, { useState } from 'react'
-import { View, Text, Button, Image } from 'react-native'
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  Button,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import Gallery from 'react-native-image-gallery';
+import SearchableDropdown from 'react-native-searchable-dropdown';
 
 export default function Home(props) {
-    const onSearch = () => props.navigation.navigate('Home Stack')
-    const [date, setDate] = useState(new Date(1598051730000));
-    const [mode, setMode] = useState('date');
-    const [show, setShow] = useState(false);
+  const onSearch = () => props.navigation.navigate('Home Stack');
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
 
-    const onChange = (event, selectedDate) => {
-        const currentDate = selectedDate || date;
-        setShow(Platform.OS === 'ios');
-        setDate(currentDate);
-    };
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
 
-    const showMode = (currentMode) => {
-        setShow(true);
-        setMode(currentMode);
-    };
+  const showMode = currentMode => {
+    setShow(true);
+    setMode(currentMode);
+  };
 
-    const showDatepicker = () => {
-        showMode('date');
-    };
+  const showDatepicker = () => {
+    showMode('date');
+  };
 
-    const showTimepicker = () => {
-        showMode('time');
-    };
+  const showTimepicker = () => {
+    showMode('time');
+  };
 
-    return (
-        <Gallery
-            style={{ flex: 1, backgroundColor: 'black' }}
-            images={[
-                { source: { uri: 'http://i.imgur.com/XP2BE7q.jpg' } },
-                { source: { uri: 'http://i.imgur.com/5nltiUd.jpg' } },
-                { source: { uri: 'http://i.imgur.com/6vOahbP.jpg' } },
-                { source: { uri: 'http://i.imgur.com/kj5VXtG.jpg' } }
-            ]}
-        />
-    )
+  const [active, setActive] = useState(0);
+
+  const dataTab = [
+    {
+      title: 'ONE WAY',
+    },
+    {
+      title: 'ROUND TRIP',
+    },
+  ];
+
+  return (
+    <>
+      <View style={{padding: 20}}>
+        <View>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignSelf: 'center',
+            }}>
+            {dataTab.map((e, i) => (
+              <TouchableOpacity
+                onPress={() => setActive(i)}
+                style={{
+                  marginHorizontal: wp(5),
+                  backgroundColor: active === i ? '#c4c4c4' : '#222',
+                  padding: 10,
+                  height: hp(5),
+                  width: wp(30),
+                  alignItems: 'center',
+                }}>
+                <Text>{e.title}</Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+          <View style={{padding: 20}}>
+            {active === 0 ? <OneWay /> : <RoundTrip />}
+          </View>
+        </View>
+      </View>
+    </>
+  );
 }
 
+const OneWay = () => {
+  return (
+    <View>
+      <Text style={{color: '#092C4C'}}>From</Text>
+      <SearchableDropdown
+        placeholder="Search place"
+        placeholderTextColor="#ABB3BB"
+        containerStyle={{borderRadius: 10}}
+        itemStyle={{borderColor: 'red'}}
+      />
+      <Text style={{color: '#092C4C'}}>To</Text>
+      <SearchableDropdown
+        placeholder="Search place"
+        placeholderTextColor="#ABB3BB"
+      />
+      <Text style={{color: '#092C4C'}}>Departure Date</Text>
+      <SearchableDropdown
+        placeholder="Search place"
+        placeholderTextColor="#ABB3BB"
+      />
+      <Text style={{color: '#092C4C'}}>Passenger</Text>
+      <SearchableDropdown
+        placeholder="Search place"
+        placeholderTextColor="#ABB3BB"
+      />
+    </View>
+  );
+};
+
+const RoundTrip = () => {
+  return (
+    <View>
+      <Text style={{color: '#092C4C'}}>From</Text>
+      <SearchableDropdown
+        placeholder="Search place"
+        placeholderTextColor="#ABB3BB"
+      />
+      <Text style={{color: '#092C4C'}}>To</Text>
+      <SearchableDropdown
+        placeholder="Search place"
+        placeholderTextColor="#ABB3BB"
+      />
+      <Text style={{color: '#092C4C'}}>Departure Date</Text>
+      <SearchableDropdown
+        placeholder="Search place"
+        placeholderTextColor="#ABB3BB"
+      />
+      <Text style={{color: '#092C4C'}}>Return Date</Text>
+      <SearchableDropdown
+        placeholder="Search place"
+        placeholderTextColor="#ABB3BB"
+      />
+      <Text style={{color: '#092C4C'}}>Passenger</Text>
+      <SearchableDropdown
+        placeholder="Search place"
+        placeholderTextColor="#ABB3BB"
+      />
+    </View>
+  );
+};
